93952737bed21801a8f479fee006e441c52a9994
Saquib02 <mohammadsaquib194@gmail.com>
2023-06-03 21:57:07 +0530

    display all the data form the database

---
+++ WebApplication1/WebApplication1/Models/Category.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication1.Models
+{
+    public class Category
+    {
+        [key]
+        public int id { get; set; }
+        [Required]
+        public string name { get; set; }
+
+        public int DisplayOrder { get; set; }
+
+        public DateTime CreatedDateTime { get; set; } = DateTime.Now;
+    }
+}
