2dc02ccb52159bf6a23084d7d95b5d0ff6392a74
Saquib02 <mohammadsaquib194@gmail.com>
2023-06-07 22:00:34 +0530

    creating crud app add validatio to it

--- WebApplication1/WebApplication1/Controllers/CategoryController.cs
+++ WebApplication1/WebApplication1/Controllers/CategoryController.cs
@@ -18,5 +18,23 @@ namespace WebApplication1.Controllers
             IEnumerable<Category> objCategoryList = _db.Categories;
             return View(objCategoryList);
         }
+        //Get
+        public IActionResult Create()
+        {
+            return View();
+        }
+        //post
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Category obj)
+        { 
+            if(ModelState.IsValid) {
+                _db.Categories.Add(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(obj);
+            
+        }
     }
 }
--- WebApplication1/WebApplication1/wwwroot/js/site.js
+++ WebApplication1/WebApplication1/wwwroot/js/site.js
@@ -1,4 +1,17 @@
 ﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
 // for details on configuring this project to bundle and minify static web assets.
 
-// Write your JavaScript code.
+// Write your JavaScript code.\
+
+
+var btn = document.getElementsByClassName(".btn1");
+
+function validate() {
+
+    var input = document.getElementsByClassName(".form-control");
+    var span = document.getElementById("er");
+    if (input == "") {
+        span.innerHTML="invalid"
+    }
+}
+
